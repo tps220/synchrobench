a5ddadfbc3cce2296b534eeee76cb230d5270b16
Thomas Salemy <tps220@lehigh.edu>
2019-04-20 16:04:29 -0400

    added ledger

--- c-cpp/src/skiplists/LazyFreeLockNuma/Set.h
+++ c-cpp/src/skiplists/LazyFreeLockNuma/Set.h
@@ -10,6 +10,7 @@ searchLayer_t** numaLayers;
 numa_allocator_t** allocators;
 int numberNumaZones;
 unsigned int levelmax;
+HazardContainer_t* memoryLedger;
 
 int sl_contains(searchLayer_t* numask, int val);
 int sl_add(searchLayer_t* numask, int val);
